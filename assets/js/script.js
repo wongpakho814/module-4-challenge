0fb74163637b49fcb8391eb073da87b26fa2c156
wongpakho814 <wongpakho814@gmail.com>
2022-09-05 17:15:06 +1000

    Added JS code for question 3-5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 var mainEl = document.querySelector(".main-content");
 
+function showAns(bool) {
+    let showAns = document.createElement('div');
+    showAns.setAttribute("class", "show-answer");
+    bool ? showAns.innerHTML = "Correct!" : showAns.innerHTML = "Wrong!";
+    return showAns;
+}
+
 function question1() {
     let question = `
-        <h1>Commonly used data types DO Not Include: </h1>
+        <h1>1. Commonly used data types DO Not Include: </h1>
         <section class="answers-btn">
             <button onclick="question2(false)">1. strings</button>
             <button onclick="question2(false)">2. booleans</button>
@@ -15,17 +22,56 @@ function question1() {
 
 function question2(bool) {
     let question = `
-        <h1>The condition in an if / else statement is enclosed with ______.</h1>
+        <h1>2. The condition in an if / else statement is enclosed with ______.</h1>
         <section class="answers-btn">
-            <button onclick="question3("false")">1. quotes</button>
-            <button onclick="question3("true")">2. curly brackets</button>
-            <button onclick="question3("false")">3. parenthesis</button>
-            <button onclick="question3("false")">4. square brackets</button>
+            <button onclick="question3(false)">1. quotes</button>
+            <button onclick="question3(false)">2. curly brackets</button>
+            <button onclick="question3(true)">3. parenthesis</button>
+            <button onclick="question3(false)">4. square brackets</button>
         </section>
     `;
-    let showAns = document.createElement('div');
-    showAns.setAttribute("class", "show-answer");
-    bool ? showAns.innerHTML = "Correct!" : showAns.innerHTML = "Wrong!";
     mainEl.innerHTML = question;
-    mainEl.appendChild(showAns);
+    mainEl.appendChild(showAns(bool));
+} 
+
+function question3(bool) {
+    let question = `
+        <h1>3. Arrays in JavaScript can be used to store ______.</h1>
+        <section class="answers-btn">
+            <button onclick="question4(false)">1. numbers and strings</button>
+            <button onclick="question4(false)">2. other arrays</button>
+            <button onclick="question4(false)">3. booleans</button>
+            <button onclick="question4(true)">4. all of the above</button>
+        </section>
+    `;
+    mainEl.innerHTML = question;
+    mainEl.appendChild(showAns(bool));
+} 
+
+function question4(bool) {
+    let question = `
+        <h1>4. String values must be enclosed within ______ when being assigned to variables.</h1>
+        <section class="answers-btn">
+            <button onclick="question5(false)">1. commas</button>
+            <button onclick="question5(false)">2. curly brackets</button>
+            <button onclick="question5(true)">3. quotes</button>
+            <button onclick="question5(false)">4. parenthesis</button>
+        </section>
+    `;
+    mainEl.innerHTML = question;
+    mainEl.appendChild(showAns(bool));
+} 
+
+function question5(bool) {
+    let question = `
+        <h1>5. A very useful tool used during development and debugging for printing content to the debugger is: </h1>
+        <section class="answers-btn">
+            <button onclick="location.href='assets/html/highscore.html'">1. JavaScript</button>
+            <button onclick="location.href='assets/html/highscore.html'">2. terminal/bash</button>
+            <button onclick="location.href='assets/html/highscore.html'">3. for loops</button>
+            <button onclick="location.href='assets/html/highscore.html'">4. console.log</button>
+        </section>
+    `;
+    mainEl.innerHTML = question;
+    mainEl.appendChild(showAns(bool));
 } 
