d456fff332969e12a7ee3feab87e9cb1a97a5e0d
wongpakho814 <wongpakho814@gmail.com>
2022-09-05 16:12:47 +1000

    Finished basic JS for question 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,17 @@
-var sitesEl = document.querySelector(".sites");
-var site1El = document.querySelector(".site1");
-var site2El = document.querySelector(".site2");
-var site3El = document.querySelector(".site3");
-// Store all h4 elements in a variable
-var siteTitles = document.querySelectorAll("h4");
+function question1() {
+    const question1 = `
+        <h1>Commonly used data types DO Not Include: </h1>
+        <section class="answers-btn">
+            <button onclick="question2()">1. strings</button>
+            <button onclick="question2()">2. booleans</button>
+            <button onclick="question2()">3. alerts</button>
+            <button onclick="question2()">4. numbers</button>
+        </section>
+    `;
+    let mainEl = document.querySelector(".main-content");
+    mainEl.innerHTML = question1;
+}
 
-site1El.children[0].textContent = "Site 1";
-site1El.children[1].setAttribute("href", "https://google.com");
-site1El.children[1].children[0].setAttribute("src", "assets/images/image_1.jpg");
-site1El.children[1].children[0].setAttribute("alt", "man working");
-site1El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-site2El.children[0].textContent = "Site 2";
-site2El.children[1].setAttribute("href", "https://twitter.com");
-site2El.children[1].children[0].setAttribute("src", "assets/images/image_2.jpg");
-site2El.children[1].children[0].setAttribute("alt", "group brainstorm");
-site2El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-site3El.children[0].textContent = "Site 3";
-site3El.children[1].setAttribute("href", "https://instagram.com");
-site3El.children[1].children[0].setAttribute("src", "assets/images/image_3.jpg");
-site3El.children[1].children[0].setAttribute("alt", "women working");
-site3El.children[1].children[0].setAttribute("style", "padding:10px;");
-
-// Loop through all h4 elements to add styling 
-for (var i = 0; i < siteTitles.length; i++) {
-  siteTitles[i].setAttribute("style", "color: blue; font-weight: bolder; font-size: 30px; padding-left:10px; margin: 0");
+function question2() {
+    
 }
