15292b7d8de982bd6af65d700917f947ad9b9b79
wongpakho814 <wongpakho814@gmail.com>
2022-09-05 16:59:52 +1000

    Finished JS for question 2, fixed positioning of various elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
+var mainEl = document.querySelector(".main-content");
+
 function question1() {
-    const question1 = `
+    let question = `
         <h1>Commonly used data types DO Not Include: </h1>
         <section class="answers-btn">
-            <button onclick="question2()">1. strings</button>
-            <button onclick="question2()">2. booleans</button>
-            <button onclick="question2()">3. alerts</button>
-            <button onclick="question2()">4. numbers</button>
+            <button onclick="question2(false)">1. strings</button>
+            <button onclick="question2(false)">2. booleans</button>
+            <button onclick="question2(true)">3. alerts</button>
+            <button onclick="question2(false)">4. numbers</button>
         </section>
     `;
-    let mainEl = document.querySelector(".main-content");
-    mainEl.innerHTML = question1;
+    mainEl.innerHTML = question;
 }
 
-function question2() {
-    
-}
+function question2(bool) {
+    let question = `
+        <h1>The condition in an if / else statement is enclosed with ______.</h1>
+        <section class="answers-btn">
+            <button onclick="question3("false")">1. quotes</button>
+            <button onclick="question3("true")">2. curly brackets</button>
+            <button onclick="question3("false")">3. parenthesis</button>
+            <button onclick="question3("false")">4. square brackets</button>
+        </section>
+    `;
+    let showAns = document.createElement('div');
+    showAns.setAttribute("class", "show-answer");
+    bool ? showAns.innerHTML = "Correct!" : showAns.innerHTML = "Wrong!";
+    mainEl.innerHTML = question;
+    mainEl.appendChild(showAns);
+} 
